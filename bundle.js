d4753e6712a08ad877b77d34b7e39249ad18a927
aivanysh <hirurg0013@gmail.com>
2017-06-03 20:30:38 +0300

    'test'

---
+++ bundle.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+d = function(){
+	console.log('foo test print 1');
+}
+
+b = function(){
+	console.log('moo test print 1');
+}
+
+function z(){
+  console.log('main test1');
+}
+
+function x(){
+	console.log('main test2');
+}
+
+exports.z = z;
+exports.x = x;
---
+++ main.js
@@ -0,0 +1,12 @@
+import d from './foo.js';
+import b from './moo.js';
+
+function z(){
+  console.log('main test1');
+}
+
+function x(){
+	console.log('main test2')
+}
+
+ export{z,x};
